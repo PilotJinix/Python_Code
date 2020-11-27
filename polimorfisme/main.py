0aa3e418b760eedbb3b72496b372c6a47b3d526d
PilotJinix <agungsantoso557@gmail.com>
2020-11-27 17:14:10 +0700

    polymorfis

---
+++ polimorfisme/main.py
@@ -0,0 +1,44 @@
+# class Person:
+#     def __init__(self, name, age):
+#         self.nama = name
+#         self.umur = age
+#
+#     def show(self):
+#         return "Nama {}\nUmur {}".format(self.nama,self.umur)
+#
+#
+# class Mahasiswa(Person):
+#     def __init__(self, name, age, nim):
+#         super().__init__(name, age)
+#         self.nim = nim
+#
+#     def show(self):
+#         return "Nama {}\nUmur {}\nNim {}".format(self.nama, self.umur, self.nim)
+#
+#
+# # orang = Person(input("Masukkan Nama : "), input("Masukkan Umur :"))
+# orang1 = Mahasiswa(input("Masukkan Nama : "), input("Masukkan Umur :"), input("Masukkan Nim :"))
+#
+# print(orang1.show())
+
+class Kalkulate:
+    # def __init__(self, X=None, Y=None, Z=None):
+    #     self.x = X
+    #     self.y = Y
+    #     self.z = Z
+
+    def mennghitung (X=None, Y=None, Z=None):
+        result= 0
+        if (X!=None and Y!=None and Z!=None ):
+            result = X + Y + Z
+        elif (X!=None and Y!=None):
+            result = X + Y
+        elif(X!=None):
+            result = X
+        else:
+            print("Butuh P")
+        return result
+
+print(Kalkulate.mennghitung(1,2))
+
+
