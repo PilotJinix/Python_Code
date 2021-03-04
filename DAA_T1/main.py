25f544a9a4ba5e35892ee20787111627347c2685
PilotJinix <agungsantoso557@gmail.com>
2021-03-04 12:47:52 +0700

    DAA_T1_Agung

---
+++ DAA_T1/main.py
@@ -0,0 +1,17 @@
+# Nomer 1
+
+a = int(input("Masukkan Nilai A = "))
+b = int(input("Masukkan Nilai B = "))
+c = int(input("Masukkan Nilai C = "))
+
+if c**2 == (a**2 + b**2):
+    print("Benar")
+else:
+    print("Salah")
+
+# Input: a
+# Input: b
+# Input: c
+
+# if c**2 == (a**2 + b**2) return true
+# else return false
